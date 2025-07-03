87694e8cbc528986d3070241f23b5543fbe9839a
vix1209 <ucheofor2015@gmail.com>
2025-07-03 13:52:17 +0100

    direct dependecy added

---
+++ dependency/dependency.go
@@ -0,0 +1,10 @@
+package dependency
+
+import (
+	Depend "github.com/Vix1209/Golang-3"
+)
+
+// Dependency demonstrates how to use a dependency from another module.
+func Dependency() {
+	Depend.DirectDependency()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Vix1209/Golang-2
 
 go 1.24.4
+
+require github.com/Vix1209/Golang-3 v0.0.0-20250703122640-7af55fb99ac7
